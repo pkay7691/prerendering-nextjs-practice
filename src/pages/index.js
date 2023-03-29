01b0cbe00679e6bec0bd9c09d01db4007933f9ac
Patrick Kenny <patrickmkenny91@gmail.com>
2023-03-29 16:31:25 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import Link from 'next/link'
 
 
 export default function Home() {
   return (
     <>
     <h1>Next JS pre-rendering</h1>
+    <Link href='/users'>Users</Link>
     </>
   )
 }
